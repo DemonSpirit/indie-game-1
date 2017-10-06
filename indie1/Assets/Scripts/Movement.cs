c49c993967680c9927279e51c280a4c8b8c503c1
Billy <wichaya@wichayas-mbp.eduroam.student>
2017-10-06 20:13:26 +0100

    Added first person movement and camera scripts. Added player object and ground plane.

---
+++ indie1/Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public CharacterController characterCtrlr;
+	public float moveSpd;
+	public Vector3 moveDirection = Vector3.zero;
+	public float jumpSpd = 16f;
+	public float gravity = 20f;
+	public Vector3 camOffset = Vector3.zero;
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		characterCtrlr = GetComponent<CharacterController> ();
+
+		camOffset = cam.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		//call fmod event : footstep.event
+
+		if (characterCtrlr.isGrounded == true) 
+		{	
+			moveDirection = new Vector3 (h, 0, v);
+			if (Input.GetButton ("Jump")) 
+			{
+				moveDirection.y = jumpSpd;
+			}
+		} else
+		{	moveDirection.x = h;
+			moveDirection.z = v;
+			moveDirection.y -= gravity*Time.deltaTime;
+		}
+			
+		moveDirection.x *= moveSpd;
+		moveDirection.z *= moveSpd;
+		moveDirection = transform.TransformDirection (moveDirection);
+		characterCtrlr.Move (moveDirection *  Time.deltaTime);
+
+		SetCameraOffset ();
+
+
+
+
+	}
+
+	void SetCameraOffset()
+	{	cam.transform.position = transform.position + camOffset;
+	}
+}
