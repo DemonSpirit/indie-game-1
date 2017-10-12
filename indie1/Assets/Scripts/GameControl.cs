2992d5823a2728356c223fe90b3a61ef44eaaa03
Wichaya Karnchanapee <wkarnchanapee@gmail.com>
2017-10-12 17:12:35 +0100

    added new scripts

---
+++ indie1/Assets/Scripts/GameControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+    int maxSteps = 1800;
+    int step = 0;
+    public string gameState = "start";
+    public float[,] inputArray = new float[1800 , 4];
+
+	// Use this for initialization
+	void Start () {
+        gameState = "live";
+        //initialise array values.
+        for (int i = 0; i < maxSteps; i++)
+        {
+            for (int ii = 0; ii < 4; ii++)
+            {
+                inputArray[i, ii] = 0;
+            }
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (gameState)
+        {   
+            case "live":
+                inputArray[step, 0] = Input.GetAxis("Horizontal");
+                inputArray[step, 1] = Input.GetAxis("Vertical");
+                step++;
+                break;
+
+            case "playback":
+                break;
+        }
+        
+    }
+}
